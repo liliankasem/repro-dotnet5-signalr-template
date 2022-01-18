f5b6b416d9ceb4e6f124745885f06d2130a9000f
Lilian Kasem <likasem@microsoft.com>
2022-01-18 10:51:47 -0800

    test app

---
+++ FunctionApp1/Function1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionApp1
+{
+    public static class Function1
+    {
+        [Function("negotiate")]
+        public static HttpResponseData Negotiate(
+            [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req,
+            [SignalRConnectionInfoInput(HubName = "")] MyConnectionInfo connectionInfo, FunctionContext context)
+        {
+            var logger = context.GetLogger("negotiate");
+            logger.LogInformation($"SignalR Connection URL = '{connectionInfo.Url}'");
+
+            var response = new HttpResponseData(HttpStatusCode.OK);
+            var headers = new Dictionary<string, string>();
+            headers.Add("Content", "Content - Type: text / html; charset = utf - 8");
+
+            response.Headers = headers;
+            response.Body = $"Connection URL = '{connectionInfo.Url}'";
+
+            return response;
+        }
+    }
+
+    public class MyConnectionInfo
+    {
+        public string Url { get; set; }
+
+        public string AccessToken { get; set; }
+    }
+}
